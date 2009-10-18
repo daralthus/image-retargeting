a4fa307a32e2dee618961cff912da1052dbbff23
vadun87 <vadun87@b3e57cf8-aa98-11de-b7bf-31c12d054fdc>
2009-10-18 20:49:52 +0000

    Saving/loading rgb32 images directly from/to desired pixel format.

--- trunk/IO.h
+++ trunk/IO.h
@@ -6,8 +6,31 @@
 
 namespace IRL
 {
-    extern const Image<RGB8> LoadImage(const std::string& path);
+    template<class PixelType>
+    const Image<PixelType> LoadImage(const std::string& path)
+    {
+        Image<PixelType> result;
+        Convert(result, LoadImage<RGB8>(path));
+        return result;
+    }
 
-    extern bool SaveImage(const Image<RGB8>& image, const std::string& path);
-    extern bool SaveGaussianPyramid(const GaussianPyramid<RGB8>& pyramid, const std::string& path);
+    template<class PixelType>
+    bool SaveImage(const Image<PixelType>& image, const std::string& path)
+    {
+        Image<RGB8> result;
+        Convert(result, image);
+        return SaveImage(result, path);
+    }
+
+    template<class PixelType>
+    bool SaveGaussianPyramid(const GaussianPyramid<PixelType>& image, const std::string& path)
+    {
+        GaussianPyramid<RGB8> result;
+        Convert(result, image);
+        return SaveGaussianPyramid(result, path);
+    }
+
+    template<> extern const Image<RGB8> LoadImage(const std::string& path);
+    template<> extern bool SaveImage(const Image<RGB8>& image, const std::string& path);
+    template<> extern bool SaveGaussianPyramid(const GaussianPyramid<RGB8>& pyramid, const std::string& path);
 }
